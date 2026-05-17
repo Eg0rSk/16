d3d6b2437825410cea5a921ab9e07356595f64fb
Eg0rSk <scurtu.egor@gmail.com>
2026-05-17 19:18:45 +0300

    Add color for button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,12 +6,14 @@ public class BtnTextChenger : MonoBehaviour
 {
     public Button buttonToChange; // кнопка, текст якої треба змінити
     private string[] randomTexts = { "Привіт", "Unity", "Кнопка", "Текст змінено" }; // масив випадкових рядків
+    private Color[] randomColors = { Color.red, Color.green, Color.yellow, Color.blue };
 
     // Метод для зміни тексту кнопки
     public void ChangeText()
     {
         string randomText = randomTexts[Random.Range(0, randomTexts.Length)]; // вибираємо випадковий рядок
         buttonToChange.GetComponentInChildren<TextMeshProUGUI>().text = randomText; // змінюємо текст кнопки
+        buttonToChange.GetComponent<Image>().color = randomColors[Random.Range(0, randomColors.Length)];
     }
 }
 
